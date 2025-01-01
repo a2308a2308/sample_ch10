c6a518c685402b0a5fb8a58f7aeb0e11d41ea2ca
lindebrocken <a2425st@aiit.ac.jp>
2025-01-01 23:43:19 +0900

    Update seeds.rb
    seed時ユーザ作成で失敗するのでコメントアウトする

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,21 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name: 'Example User',
-             email: 'example@railstutorial.org',
-             password: 'foobar',
-             password_confirmation: 'foobar',
-             admin: true)
+#User.create!(name: 'Example User',
+#             email: 'example@railstutorial.org',
+#             password: 'foobar',
+#             password_confirmation: 'foobar',
+#             admin: true)
 
 # 追加のユーザーをまとめて生成する
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
+#99.times do |n|
+#  name  = Faker::Name.name
+#  email = "example-#{n + 1}@railstutorial.org"
+#  password = 'password'
+#  User.create!(name: name,
+#               email: email,
+#               password: password,
+#               password_confirmation: password)
+#end
+
 # サンプル質問を作成
 question = Question.find_or_create_by(
   subject: 'サンプル質問',
