ad7b880d0a5733fc3931b837a20ceef3777ffd31
lindebrocken <a2425st@aiit.ac.jp>
2025-01-01 23:56:04 +0900

    Update render-build.sh
    add db:migrate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# tanaka add db:migrate
+bundle exec rails db:migrate
 bundle exec rails db:seed
