e50872ba36644724ce13e6d0d29bdf1bc029a511
Haruki Kataoka <a2412hk@aiit.ac.jp>
2025-01-05 21:38:00 +0900

    Fix #.where(deleted_at: nil) # 削除されていないユーザーを対象

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -2,7 +2,7 @@ class Ranking < ApplicationRecord
     # ランキングデータを取得するメソッド
     def self.top_ranking(limit = 10)
       User.joins(:scores)
-          .where(deleted_at: nil) # 削除されていないユーザーを対象
+          #.where(deleted_at: nil) # 削除されていないユーザーを対象
           .select('users.name AS user_name, SUM(scores.total) AS total_score') # totalを使う
           .group('users.id, users.name')
           .order('total_score DESC')
