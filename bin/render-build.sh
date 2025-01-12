368e3309eba02b48b2caa92d940b40a98c86822a
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 20:36:28 +0000

    delete uneed echos(a2308 added)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-echo $SCHEMA_NAME
-
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
@@ -11,5 +9,4 @@ DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 psql $DATABASE_URL -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME;"
 # tanaka add db:migrate
 bundle exec rails db:migrate
-# bundle exec rails db:migrate
 bundle exec rails db:seed
