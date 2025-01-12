4ef324164815f612792f920005c2f55eb8ad48c1
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 19:46:30 +0000

    add create SCHEMA to render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -13,6 +13,15 @@ bundle exec rails assets:clean
 echo doing DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 
+# oda add schema create
+# 環境変数 SCHEMA_NAME を確認
+if [ -z "$SCHEMA_NAME" ]; then
+  echo "SCHEMA_NAME is not set."
+  exit 1
+fi
+# スキーマを作成
+psql -d データベース名 -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME;"
+
 # tanaka add db:migrate
 echo doing rails db:migrate
 bundle exec rails db:migrate
