86f63b890124a46bca59845e2c52ee11ca9b1796
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 17:33:18 +0000

    add render-build.sh db:drop db:create

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,6 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 # tanaka add db:migrate
-bundle exec rails db:migrate
+bundle rails db:drop db:create db:migrate
+# bundle exec rails db:migrate
 bundle exec rails db:seed
