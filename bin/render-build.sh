408e9e4d5ecb0cae2f4e59cf9653a648ba309ced
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 18:12:54 +0000

    add echos

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-echo $SCHEMA_NAME
 bundle install
+echo doing rails assets:precompile
 bundle exec rails assets:precompile
+
+echo doing exec rails assets:clean
 bundle exec rails assets:clean
+
+echo doing DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+
 # tanaka add db:migrate
+echo doing rails db:drop db:create db:migrate
 bundle rails db:drop db:create db:migrate
+
 # bundle exec rails db:migrate
+echo doing rails db:seed
 bundle exec rails db:seed
