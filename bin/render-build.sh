d3c14c7cde7940ce1bcf3e81151e49b457cd5318
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 20:26:55 +0000

    delete uneed echos(a2308 added)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,16 +4,11 @@ set -o errexit
 echo $SCHEMA_NAME
 
 bundle install
-echo doing rails assets:precompile
 bundle exec rails assets:precompile
-
-echo doing exec rails assets:clean
 bundle exec rails assets:clean
-
-echo doing DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 
-# oda add schema create
+# a2308 add schema create
 # スキーマを作成
 psql $DATABASE_URL -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME;"
 
