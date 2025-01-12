84d916f8ef4711078e02893db5950ca7c0123208
a2308a2308 <a2308ho@aiit.ac.jp>
2025-01-12 19:53:07 +0000

    change db name to create schema

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -14,13 +14,8 @@ echo doing DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:cre
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 
 # oda add schema create
-# 環境変数 SCHEMA_NAME を確認
-if [ -z "$SCHEMA_NAME" ]; then
-  echo "SCHEMA_NAME is not set."
-  exit 1
-fi
 # スキーマを作成
-psql -d quiz-app-team4_DB -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME;"
+psql -d "quiz-app-team4_DB" -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME;"
 
 # tanaka add db:migrate
 echo doing rails db:migrate
